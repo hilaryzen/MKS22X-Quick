5f8005b13ba576a942f828793f00994344a9689b
Hilary Zen <hzen00@stuy.edu>
2019-03-12 02:01:25 -0400

    Added to driver

--- Driver.java
+++ Driver.java
@@ -1,8 +1,20 @@
+import java.util.Arrays;
+
 public class Driver {
   public static void main(String[] args) {
     int[] data = new int[Integer.parseInt(args[0])];
     for (int i = 0; i < data.length; i++) {
       data[i] = (int)(Math.random() * 100);
+    }
+
+    int k = (int)(Math.random() * data.length);
+    int selected = Quick.quickselect(data, k);
+
+    Arrays.sort(data);
+    if (data[k] == selected) {
+      System.out.println("Passed: " + data[k]);
+    } else {
+      System.out.println("Failed: selected = " + selected + ", data[k] = " + data[k]);
     }
   }
 }
