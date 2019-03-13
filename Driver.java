9ae5b6fa2a11ec535e8bccde2d5f3aaef40465a8
Hilary Zen <hzen00@stuy.edu>
2019-03-13 10:57:29 -0400

    Edited driver

--- Driver.java
+++ Driver.java
@@ -8,7 +8,9 @@ public class Driver {
     }
     //System.out.println(Arrays.toString(data));
 
-    if (args[1] == "select") {
+    //if (args[1] == "select") {
+
+    /*
       //int k = 0;
       int k = (int)(Math.random() * data.length);
       System.out.println("k = " + k);
@@ -21,13 +23,21 @@ public class Driver {
       } else {
         System.out.println("Select failed: selected = " + selected + ", data[k] = " + data[k]);
       }
-    } else if (args[1] == "sort") {
+    */
+
+    //} else if (args[1] == "sort") {
+
+      //Making second copy of array
       int[] data2 = new int[data.length];
       for (int i = 0; i < data.length; i++) {
         data2[i] = data[i];
       }
-      quicksort(data, 0, data.length - 1);
+      Quick.quicksort(data, 0, data.length - 1);
       Arrays.sort(data2);
+
+      System.out.println(Arrays.toString(data));
+      System.out.println(Arrays.toString(data2));
+
       for (int i = 0; i < data.length; i++) {
         if (data[i] != data2[i]) {
           System.out.println("Sort failed: i = " + i + ", data[i] = " + data[i] + ", data2[i] = " + data2[i]);
@@ -36,6 +46,7 @@ public class Driver {
           System.out.println("Sort passed");
         }
       }
-    }
+
+    //}
   }
 }
