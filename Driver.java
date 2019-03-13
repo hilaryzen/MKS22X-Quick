648ed0806cf586d440f29fbc6a83e4f68965081f
Hilary Zen <hzen00@stuy.edu>
2019-03-13 10:46:33 -0400

    Adding to driver

--- Driver.java
+++ Driver.java
@@ -8,17 +8,21 @@ public class Driver {
     }
     //System.out.println(Arrays.toString(data));
 
-    //int k = 0;
-    int k = (int)(Math.random() * data.length);
-    System.out.println("k = " + k);
-    int selected = Quick.quickselect(data, k);
+    if (args[1] == "select") {
+      //int k = 0;
+      int k = (int)(Math.random() * data.length);
+      System.out.println("k = " + k);
+      int selected = Quick.quickselect(data, k);
 
-    Arrays.sort(data);
-    //System.out.println("Sorted array: " + Arrays.toString(data));
-    if (data[k] == selected) {
-      System.out.println("Passed: selected = " + selected + ", data[k] = " + data[k]);
-    } else {
-      System.out.println("Failed: selected = " + selected + ", data[k] = " + data[k]);
+      Arrays.sort(data);
+      //System.out.println("Sorted array: " + Arrays.toString(data));
+      if (data[k] == selected) {
+        System.out.println("Select passed: selected = " + selected + ", data[k] = " + data[k]);
+      } else {
+        System.out.println("Select failed: selected = " + selected + ", data[k] = " + data[k]);
+      }
+    } else if (args[1] == "sort") {
+      quicksort(data, 0, data.length - 1);
     }
   }
 }
